48f14e83e6a4adf01fdb87756bd731024053ae89
Vladimir <fayst_@bk.ru>
2020-08-14 17:36:36 +0300

    Реализовал функционал выполнения задачи(зачеркивает текст), выделние задачи как важное(меняет цвет текста и выделяет жирным текстом) и удаление задачи(удаляет задачу из списка).

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Head from '../head';
 import SearchPanel from '../searchPanel';
 import TodoList from '../todoList';
 import ItemStatusFilter from '../itemStatusFilter';
 import './app.css';
 
-const App = () => {
-  const todoData = [
+export default class App extends Component {
+  state = {
+    todoData: [
     {
       label: 'Выпить кофе',
       important: false,
@@ -22,18 +23,34 @@ const App = () => {
       important: false,
       id: 3
     },
-];
+  ]};
 
-  return (
-    <div className='todo-app'>
-      <Head toDo={1} done={3}/>
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
-      </div> 
-      
-      <TodoList todos={ todoData } />   
-    </div>
-  );
+  deleteItem = id => {
+
+    this.setState(({ todoData }) => {
+     const newTodoData = todoData.filter(el => {
+       return el.id !== id;
+     });
+
+     return { todoData: newTodoData }
+    });
+  };
+
+  render () {
+    const { todoData } = this.state;
+
+    return (
+      <div className='todo-app'>
+        <Head toDo={1} done={3}/>
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div> 
+        
+        <TodoList 
+          todos = { todoData }
+          onDeleted = { this.deleteItem } />   
+      </div>
+    );
+  }  
 };
-export default App;
--- src/components/todoListItem/TodoListItem.js
+++ src/components/todoListItem/TodoListItem.js
@@ -3,32 +3,65 @@ import React, { Component } from 'react';
 import './todoListItem.css';
 
 export default class TodoListItem extends Component {
+ 
+  //Локальное состояние
+  state = {
+    done: false,
+    important: false
+  }
+
+
+  // Обрабочики событий
+  onHendlerClickListItem = (evt) => {
+    evt.preventDefault();
+
+    this.setState(({ done }) => {
+      return {
+        done: !done
+      };
+    });
+  };
+
+  onMarkImportant = (evt) => {
+    evt.preventDefault();
+
+    this.setState(({ important }) => {
+      return {
+        important: !important
+      }
+    });
+  };
 
   render () {
 
-    const { label, important } = this.props;
+    const { label, onDeleted } = this.props;
+    const { done, important } = this.state;
 
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+    let className = 'todo-list-item';
+
+    if (done) className += ' done';
+    if (important) className += ' important';
+
+    
 
     return (
-      <span className="todo-list-item">
+      <span className={ className }>
         <span
-          className="todo-list-item-label"
-          style={style}>
-          {label}
+          className='todo-list-item-label'
+          onClick = { this.onHendlerClickListItem }>
+            { label }         
         </span>
 
         <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
+                className="btn btn-outline-success btn-sm float-right"
+                onClick= { this.onMarkImportant }>
           <i className="fa fa-exclamation" />
         </button>
 
         <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
-          <i className="fa fa-trash-o" />
+          className="btn btn-outline-danger btn-sm float-right"
+          onClick = { onDeleted }>
+            <i className="fa fa-trash-o" />
         </button>
       </span>
     );
