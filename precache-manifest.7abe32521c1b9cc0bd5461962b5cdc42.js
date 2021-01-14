b774a9e72c59ba1e92684f0021bbfa99219a5f3c
Vladimir <fayst_@bk.ru>
2021-01-14 21:14:15 +0300

    Updates

---
+++ precache-manifest.7abe32521c1b9cc0bd5461962b5cdc42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a6cdb00f68a31b8dbe0e50fee09136d",
+    "url": "/React_ToDo_List/index.html"
+  },
+  {
+    "revision": "14e64b70d252c5eb3186",
+    "url": "/React_ToDo_List/static/css/main.21fe8e86.chunk.css"
+  },
+  {
+    "revision": "16e31a139d2dc7bb4f03",
+    "url": "/React_ToDo_List/static/js/2.a278dd83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_ToDo_List/static/js/2.a278dd83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14e64b70d252c5eb3186",
+    "url": "/React_ToDo_List/static/js/main.f593be24.chunk.js"
+  },
+  {
+    "revision": "315bb4b51c2decd16725",
+    "url": "/React_ToDo_List/static/js/runtime-main.63b6ec89.js"
+  }
+]);
